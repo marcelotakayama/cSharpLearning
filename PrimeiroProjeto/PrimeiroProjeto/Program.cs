eb7ce160eb9087436736769bea57e4916cbce32a
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-05 10:55:52 -0300

    Estrutura de repetição FOR

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,16 +4,16 @@ using System.Globalization;
 namespace PrimeiroProjeto {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Digite um número: ");
-            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos números você vai digitar: ");
+            int N = int.Parse(Console.ReadLine());
 
-            while (x >= 0.0) {
-                double raiz = Math.Sqrt(x);
-                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
-                Console.Write("Digite outro número: ");
-                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int soma = 0;
+            for (int i = 1; i <= N; i++) {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+                Console.WriteLine("Soma = "+ soma);
             }
-            Console.WriteLine("Número negativo");
         }
     }
 }

01d1aa97d9dbfc738b5c531ab2517b26a84f0cfb
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-05 11:10:40 -0300

    Triângulo com maior área

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,15 +4,30 @@ using System.Globalization;
 namespace PrimeiroProjeto {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Quantos números você vai digitar: ");
-            int N = int.Parse(Console.ReadLine());
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Digite os três lados do triângulo X: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            int soma = 0;
-            for (int i = 1; i <= N; i++) {
-                Console.Write("Valor #{0}: ", i);
-                int valor = int.Parse(Console.ReadLine());
-                soma += valor;
-                Console.WriteLine("Soma = "+ soma);
+            Console.WriteLine("Digite os três lados do triângulo Y: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY) {
+                Console.WriteLine("Maior área = X");
+            } else{
+                Console.WriteLine("Maior área = Y");
             }
         }
     }

49f587e5aec18a1a93d26531e854c92a415e6c31
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-05 15:34:41 -0300

    Construtores

--- PrimeiroProjeto/PrimeiroProjeto/Produto.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Produto.cs
@@ -9,6 +9,12 @@ namespace PrimeiroProjeto {
         public double Preco;
         public int Quantidade;
 
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
         public double ValorTotalEmEstoque() {
             return Preco * Quantidade;
         }
--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -7,7 +7,29 @@ namespace PrimeiroProjeto {
         
         
         static void Main(string[] args) {
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estorque: ");
+            int quantidade = int.Parse(Console.ReadLine());
 
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite o número de produtos a serem adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("\nDados atualizados: " + p);
+
+            Console.Write("Digite o número de produtos a serem removidos no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine("\nDados atualizados: " + p);
+
+            /*
             Console.Write("Digite o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             
@@ -17,6 +39,7 @@ namespace PrimeiroProjeto {
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Valor de PI: "+ Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            */
         }
     }
 }

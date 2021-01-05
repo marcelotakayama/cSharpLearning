4f104efccaffef0db0b7145cdf7b75d342350303
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-05 11:56:53 -0300

    Resolução do problema da área do maior triângulo com classes

--- PrimeiroProjeto/PrimeiroProjeto/Triangulo.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Triangulo.cs
@@ -1,7 +1,14 @@
-﻿namespace PrimeiroProjeto {
+﻿using System;
+
+namespace PrimeiroProjeto {
     class Triangulo {
         public double A;
         public double B;
         public double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }

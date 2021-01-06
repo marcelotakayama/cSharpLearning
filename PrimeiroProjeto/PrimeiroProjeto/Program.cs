bf938a4e9a72b16ab431b849edea077c3f0efdd2
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-06 11:59:16 -0300

    Nullable parte 1

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -3,17 +3,25 @@ using System.Globalization;
 
 namespace PrimeiroProjeto {
     class Program {
-
-        
-        
         static void Main(string[] args) {
-            Produto p = new Produto("TV", 900, 10);
+            double? x = null;
+            double? y = 10.0;
 
-            p.Nome = ("TV 4K");
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
 
-            Console.WriteLine(p.Nome);
-            Console.WriteLine(p.Preco);
-            Console.WriteLine(p.Quantidade);
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+            
+            if(y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
         }
     }
 }

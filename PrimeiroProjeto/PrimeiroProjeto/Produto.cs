19ac2510895ccdff2c478d4c53805e0456375b5c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-06 10:48:36 -0300

    Auto properties

--- PrimeiroProjeto/PrimeiroProjeto/Produto.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Produto.cs
@@ -6,20 +6,20 @@ using System.Globalization;
 namespace PrimeiroProjeto {
     class Produto {
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         public Produto() {
-            _quantidade = 10;
+            Quantidade = 10;
         }
 
         public Produto(string nome, double preco) : this() {
             _nome = nome;
-            _preco = preco;
+            Preco = preco;
         }
 
         public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
-            _quantidade = quantidade;
+            Quantidade = quantidade;
         }
 
         public string Nome {
@@ -31,31 +31,24 @@ namespace PrimeiroProjeto {
             }
         }
 
-        public double Preco {
-            get { return _preco; }
-        }
-
-        public int Quantidade {
-            get { return _quantidade; }
-        }
 
         public double ValorTotalEmEstoque() {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }        
         public void RemoverProdutos(int quantidade) {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
 
         public override string ToString() {
             return _nome
                 + ", $"
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + _quantidade
+                + Quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }

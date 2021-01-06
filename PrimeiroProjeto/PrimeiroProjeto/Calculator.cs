5172b28c3d556576a5e2fc2f9f94400c44aec7b7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-06 18:29:20 -0300

    Depois eu termino

--- PrimeiroProjeto/PrimeiroProjeto/Calculator.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Calculator.cs
@@ -4,12 +4,8 @@ using System.Text;
 
 namespace PrimeiroProjeto {
     class Calculator {
-        public static int Sum(params int[] numbers) {
-            int sum = 0;
-            for (int i=0; i<numbers.Length; i++) {
-                sum += numbers[i]; 
-            }
-            return sum;
+        public static void Triple(int origin, out int result) {
+            result = origin;
         }
     }
 }
--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,11 +4,9 @@ using System.Globalization;
 namespace PrimeiroProjeto {
     class Program {
         static void Main(string[] args) {
-            int s1 = Calculator.Sum( 2, 3 );
-            int s2 = Calculator.Sum( 2, 4, 3 );
-
-            Console.WriteLine(s1);
-            Console.WriteLine(s2);
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
         }
     }
 }

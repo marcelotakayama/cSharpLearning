0cf44c8c1af778f9e316ecd27316e24f040a7907
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 15:36:58 -0300

    Subindo pasta e inicio de inferencias

---
+++ AulaInferencia/AulaInferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaInferencia {
+    class Program {
+        static void Main(string[] args) {
+
+            var x = 10;
+            var y = 10.0;
+            var z = "Marcelo";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}

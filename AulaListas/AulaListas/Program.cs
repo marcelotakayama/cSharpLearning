d099577988aa8532de8c176a6ea0e930537120c7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 15:05:12 -0300

    Parte 2 de listas

--- AulaListas/AulaListas/Program.cs
+++ AulaListas/AulaListas/Program.cs
@@ -9,7 +9,7 @@ namespace AulaListas {
         static void Main(string[] args) {
             List<string> list = new List<string>();
 
-            list.Add("Marcelo"); 
+            list.Add("Maria"); 
             list.Add("Alex"); 
             list.Add("Bob");
             list.Add("Ana");
@@ -27,6 +27,34 @@ namespace AulaListas {
 
             string s2 = list.FindLast(x => x[0] == 'A');
             Console.WriteLine("Last 'A': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position 'A': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position 'A': " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("----------------");
+
+            foreach (string obj in list2) {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAt(2);
+
+            Console.WriteLine("------------------");
+
+            foreach(string obj in list) {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("------------------");
+
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
         }
     }
 }

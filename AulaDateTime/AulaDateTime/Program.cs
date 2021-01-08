f2f9c9f5ad6419d1c3c8033f66d82a462c9f4ecc
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 17:41:47 -0300

    subindo pra corrigir erros

--- AulaDateTime/AulaDateTime/Program.cs
+++ AulaDateTime/AulaDateTime/Program.cs
@@ -8,8 +8,18 @@ namespace AulaDateTime {
     class Program {
         static void Main(string[] args) {
             DateTime d1 = DateTime.Now;
-            Console.WriteLine(d1);
+            DateTime d2 = new DateTime(2021, 1, 8);
+            DateTime d3 = new DateTime(2021, 1, 8, 17, 36, 12);
+            DateTime d4 = new DateTime(2021, 1, 8, 17, 36, 12, 500);
+
+            DateTime d5 = new DateTime.Now;
+
+
+
             Console.WriteLine(d1.Ticks);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
         }
     }
 }

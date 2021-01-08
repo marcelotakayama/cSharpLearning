d4f4d72951dde7465644830e9eb7524300dec8cf
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 12:01:17 -0300

    Arrumando bug na variavel triple

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -5,8 +5,9 @@ namespace PrimeiroProjeto {
     class Program {
         static void Main(string[] args) {
             int a = 10;
-            Calculator.Triple(ref a);
-            Console.WriteLine(a);
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
         }
     }
 }

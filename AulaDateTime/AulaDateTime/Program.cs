216ccbb0a079dff4af6980c66249a6f21eb059ce
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 18:02:23 -0300

    Usando parseExact

--- AulaDateTime/AulaDateTime/Program.cs
+++ AulaDateTime/AulaDateTime/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace AulaDateTime {
     class Program {
@@ -11,12 +12,14 @@ namespace AulaDateTime {
             DateTime d2 = new DateTime(2021, 1, 8);
             DateTime d3 = new DateTime(2021, 1, 8, 17, 36, 12);
             DateTime d4 = new DateTime(2021, 1, 8, 17, 36, 12, 500);
-
             DateTime d5 = DateTime.Now;
             DateTime d6 = DateTime.Today;
             DateTime d7 = DateTime.UtcNow;
-
-
+            DateTime d8 = DateTime.Parse("2000-08-15");
+            DateTime d9 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d10 = DateTime.Parse("15/08/2000");
+            DateTime d11 = DateTime.Parse("15/08/2000 13:05:58");
+            DateTime d12 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
 
             Console.WriteLine(d1.Ticks);
             Console.WriteLine(d1);
@@ -26,6 +29,10 @@ namespace AulaDateTime {
             Console.WriteLine(d5);
             Console.WriteLine(d6);
             Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
         }
     }
 }

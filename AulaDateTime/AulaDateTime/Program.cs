9848c58a968c163ddfe8524e5d037616f526d5da
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 17:29:11 -0300

    Inicio do conteudo de datetime

--- AulaDateTime/AulaDateTime/Program.cs
+++ AulaDateTime/AulaDateTime/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace AulaDateTime {
     class Program {
         static void Main(string[] args) {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
         }
     }
 }

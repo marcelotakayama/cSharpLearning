1caf41b9e35457d8aff85744a64fde0fb9e8378c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 12:45:04 -0300

    Iniciando estudos sobre listas

--- AulaListas/AulaListas/Program.cs
+++ AulaListas/AulaListas/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace AulaListas {
     class Program {
         static void Main(string[] args) {
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string> { "Maria", "Alex" };
         }
     }
 }

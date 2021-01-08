254922c85099ff1803b6e92b25724ff87c84be74
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 15:30:01 -0300

    iniciando com matrizes

--- AulaMatriz/AulaMatriz/Program.cs
+++ AulaMatriz/AulaMatriz/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace AulaMatriz {
     class Program {
         static void Main(string[] args) {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
         }
     }
 }

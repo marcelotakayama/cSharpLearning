4df19a100aefcdfe0e3ecc95bffdbf2ac8f3de36
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 16:03:09 -0300

    tentando arrumas os b.o

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,37 +8,10 @@ using System.Threading.Tasks;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            int x = int.Parse(Console.ReadLine());
-            string day;
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0)?preco *0.1: preco*0.05;
 
-            switch (x) {
-                case 1:
-                    day = "Sunday";
-                    break;
-                case 2:
-                    day = "Monday";
-                    break;
-                case 3:
-                    day = "Tuesday";
-                    break;
-                case 4:
-                    day = "Wednesday";
-                    break;
-                case 5:
-                    day = "Thursday";
-                    break;
-                case 6:
-                    day = "Friday";
-                    break;
-                case 7:
-                    day = "Saturday";
-                    break;
-                default:
-                    day = "Invalid value";
-                    break;
-            }
-
-            Console.WriteLine("Day: " + day);
+            Console.WriteLine(desconto);
         }
     }
 }

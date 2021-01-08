df2a3832ea3cf22eed1c9dd0d40f2a24cac00d57
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-08 16:06:20 -0300

    teste

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -10,7 +10,7 @@ namespace Course {
         static void Main(string[] args) {
             double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             double desconto = (preco < 20.0)?preco *0.1: preco*0.05;
-
+            //teste
             Console.WriteLine(desconto);
         }
     }

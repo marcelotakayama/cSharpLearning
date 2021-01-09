21e287ab80ba0dc3953c740b31f0e19998e95c2a
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-09 10:43:36 -0300

    Upando aula de TimeSpan

--- AulaTimeSpan/AulaTimeSpan/Program.cs
+++ AulaTimeSpan/AulaTimeSpan/Program.cs
@@ -3,7 +3,20 @@
 namespace AulaTimeSpan {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(2, 11, 21);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+            TimeSpan t6 = TimeSpan.FromDays(2.5);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
         }
     }
 }

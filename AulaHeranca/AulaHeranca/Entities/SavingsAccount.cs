c42defd63bca54ddb6d3ae17fc0ce02a312c25f7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-11 17:41:08 -0300

    Terminando o interestRate

--- AulaHeranca/AulaHeranca/Entities/SavingsAccount.cs
+++ AulaHeranca/AulaHeranca/Entities/SavingsAccount.cs
@@ -9,7 +9,15 @@ namespace AulaHeranca.Entities {
         public double InterestRate { get; set; }
 
         public SavingsAccount() {
+        }
 
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance) {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance() {
+            Balance += Balance * InterestRate;
         }
     }
 }

05649121724e6f60517013d0e333125b5896d622
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-11 17:31:17 -0300

    Subindo a pasta de entidades

---
+++ AulaHeranca/AulaHeranca/Entities/SavingsAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaHeranca.Entities {
+    class SavingsAccount : Account {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() {
+
+        }
+    }
+}

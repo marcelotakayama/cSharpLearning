ab179f34e5844048e92f497676f12fbf4fca2f5c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-11 11:54:22 -0300

    Mexendo com enumeração em C#

---
+++ ProjetoIntermediario/ProjetoIntermediario/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ProjetoIntermediario.Entities.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIntermediario.Entities {
+    class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
--- ProjetoIntermediario/ProjetoIntermediario/Program.cs
+++ ProjetoIntermediario/ProjetoIntermediario/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjetoIntermediario.Entities;
+using ProjetoIntermediario.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,14 @@ using System.Threading.Tasks;
 namespace ProjetoIntermediario {
     class Program {
         static void Main(string[] args) {
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+            Console.WriteLine(order);
         }
     }
 }

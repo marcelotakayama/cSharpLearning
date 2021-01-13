b4f8bb4f7e4401bb4bb7f52b9da666e966b58df8
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-13 16:23:08 -0300

    aprendendo sobre try catch

---
+++ AulaTryCatch/AulaTryCatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaTryCatch {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            } catch(DivideByZeroException e) {
+                Console.WriteLine("Divisão por zero não é permitida");
+            } catch (FormatException e) {
+                Console.WriteLine("Format Error! " + e.Message);
+            }
+        }
+    }
+}

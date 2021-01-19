39f8acb5e580f13fa780c184306e7a7369a95612
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 10:38:55 -0300

    ToString do invoice implementado

---
+++ AulaInterfaces/AulaInterfaces/Entities/Invoice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AulaInterfaces.Entities {
+    class Invoice {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax) {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString() {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "Total payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

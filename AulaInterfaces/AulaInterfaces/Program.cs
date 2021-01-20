c84ce73541a5c3841c095d74174df7bb479398d3
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 09:39:47 -0300

    Implementando o programa principal do projeto

--- AulaInterfaces/AulaInterfaces/Program.cs
+++ AulaInterfaces/AulaInterfaces/Program.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using AulaInterfaces.Entities;
 
 namespace AulaInterfaces {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicule(model));
         }
     }
 }
